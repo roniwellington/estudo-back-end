9ef59d406e312629d6b8ead5e4d04318b208b555
Roni Wellington <roniwellington@gmail.com>
2023-03-07 17:46:22 -0300

    estudo back end

---
+++ algoritmos/menorValor.js
@@ -0,0 +1,18 @@
+const livros = require('./listaLivro');
+
+function menorValor(arrProdutos, posicaoInicial) {
+
+    let maisBarato = posicaoInicial;
+
+    for (let atual = 0; atual < arrProdutos.length; atual++) {
+        if (arrProdutos[atual].preco < arrProdutos[maisBarato].preco) {
+            maisBarato = atual
+        }
+    }
+
+    return maisBarato;
+}
+
+
+
+module.exports = menorValor;
