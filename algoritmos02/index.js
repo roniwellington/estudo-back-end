b37e2320da29c4589964b937f9f3bf71e02ce191
Roni Wellington <roniwellington@gmail.com>
2023-03-08 09:47:20 -0300

    estudo de algoritmos

---
+++ algoritmos02/index.js
@@ -0,0 +1,14 @@
+const {edGalho, edFolha} = require('./arrays')
+
+function juntaListas(lista1, lista2){
+    let listaFinal = [];
+    let posicaoAtualLista1 = 0;
+    let posicaoAtualLista2 = 0;
+
+    while (posicaoAtualLista1 < lista1.length && posicaoAtualLista2 < lista2.length){
+        let produtoAtualLista1 = lista1[posicaoAtualLista1];
+        let produtoAtualLista2 = lista2[posicaoAtualLista2];
+    }
+
+    return listaFinal;
+}
