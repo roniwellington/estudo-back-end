e2ecc7bbec0eebe8f4179cd61dd4d01355c75de7
Roni Wellington <roniwellington@gmail.com>
2023-03-09 08:57:03 -0300

    estudo back-end

--- algoritmos02/aula02/mergeSort.js
+++ algoritmos02/aula02/mergeSort.js
@@ -1,10 +1,14 @@
-const listaLivros = require('./arrays');
+const listaLivros = require('./array');
 
-function mergeSort(array){
+function mergeSort(array, nivelAninhamento = 0){
+
+    console.log(`Nível de aninhamento ${nivelAninhamento}`)
+    console.log(array)
+
     if(array.length > 1) {
         const meio = Math.floor(array.length / 2)
-        const parte1 = mergeSort(array.slice(0, meio));
-        const parte2 = mergeSort(array.slice(meio, array.length));
+        const parte1 = mergeSort(array.slice(0, meio), nivelAninhamento + 1);
+        const parte2 = mergeSort(array.slice(meio, array.length), nivelAninhamento + 1);
         array = ordena(parte1, parte2);
 
     }
@@ -13,6 +17,7 @@ function mergeSort(array){
 }
 
 function ordena(parte1, parte2){
+
     let posicaoAtualParte1 = 0;
     let posicaoAtualParte2 = 0;
     const resultado = []
