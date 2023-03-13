02331b2d372fb0880426e12837261d81cf8734c5
Roni Wellington <roniwellington@gmail.com>
2023-03-13 11:32:10 -0300

    estudo de back end com mongoose e mongobd

--- alura-node/src/controllers/livrosController.js
+++ alura-node/src/controllers/livrosController.js
@@ -3,21 +3,25 @@ import livros from "../models/Livro.js";
 class LivroController {
 
     static listarLivros = (req, res) => {
-        livros.find((err, livros) => {
-            res.status(200).json(livros);
-        })
+        livros.find()
+            .populate('autor')
+            .exec((err, livros) => {
+                res.status(200).json(livros);
+            })
     }
 
     static listarLivroPorId = (req, res) => {
         const id = req.params.id;
 
-        livros.findById(id, (err, livros) => {
-            if (err) {
-                res.status(400).send({ message: `${err.message} - Id do livro não localizado.` })
-            } else {
-                res.status(200).send(livros);
-            }
-        })
+        livros.findById(id)
+            .populate('autor', 'nome')
+            .exec((err, livros) => {
+                if (err) {
+                    res.status(400).send({ message: `${err.message} - Id do livro não localizado.` })
+                } else {
+                    res.status(200).send(livros);
+                }
+            })
     }
 
     static cadastrarLivro = (req, res) => {
@@ -50,10 +54,18 @@ class LivroController {
 
         livros.findByIdAndDelete(id, (err) => {
             if (!err) {
-                res.status(200).send({message: 'Livro removido com sucesso'})
+                res.status(200).send({ message: 'Livro removido com sucesso' })
             } else {
-                res.status(500).send({message: err.message})
+                res.status(500).send({ message: err.message })
             }
+        })
+    }
+
+    static listarLivrosPorEditora = (req, res) => {
+        const editora = req. query.editora
+
+        livros.find({'editora': editora}, {}, (err, livros) => {
+            res.status(200).send(livros);
         })
     }
 }
